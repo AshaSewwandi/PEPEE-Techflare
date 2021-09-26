6b7475d170c4a3877d2ef2b1b1aa7be2c6ceaa73
lakminifernando77 <lakminifernando77@gmail.com>
2021-09-26 14:42:19 +0530

    DT-9,DT-10,DT-11- Landing page updated

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -51,9 +51,9 @@
           <div class="item3">
             <h1><u>About Us</u><img src="/img/img003.png" alt="" width="100" height="100"> </h1>
           </div>
-          <div class="item4">
-            <div id="Frame-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-          <div class="image">
+            <div class="item4">
+              <div id="Frame-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            <div class="image">
             <img src="/img/img001.png" alt="" width="100" height="100">
           </div>
         </div>
