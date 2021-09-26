ec62ac1fb218fa2cb8add9d893a2a16b192456be
Sewwandi97 <asha.18@itfac.mrt.ac.lk>
2021-09-26 17:30:32 +0530

    DT-30 Landing page update

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -3,9 +3,6 @@
     <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
       <link href="/css/style.css" rel="stylesheet">   
     </head>
     <body>
@@ -53,9 +50,10 @@
           </div>
             <div class="item4">
               <div id="Frame-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-            <div class="image">
-            <img src="/img/img001.png" alt="" width="100" height="100">
-          </div>
+              <div class="image">
+                <img src="/img/img001.png" alt="" width="100" height="100">
+              </div>
+            </div>
         </div>
       </div>
     </section>     
@@ -66,52 +64,47 @@
             <h1><u> Our Services</u> <img src="/img/img003.png" alt="" width="100" height="100"> </h1>
           </div>
           <div class="item6">
-          <ul style="list-style-type:none;">
+            <ul style="list-style-type:none;">
               <li>Find homes for pets</li>
               <li>Find pets for adoption</li>
               <li>Tips to take care of your pets</li>
               <li>Pet Care Center</li>
             </ul> 
           </div>
-        <div class="section2-image3">
+        <div class="section2-image3"></div>
       </div>
     </section>   
     <section id="section4" class="parallax" data-stellar-background-ratio="0.5">
       <div class="container">
-				<div class="caption text-center text-white" data-stellar-ratio="0.7">       
+        <div class="caption text-center text-white" data-stellar-ratio="0.7">       
           <div class="item7">
             <h1>Get in Touch </h1>
           </div>
-          <div class="container">
-            <div class="row">
-              <div class="col-sm">
-                <form>
-                  <div class="form-group">
-                    <label for="exampleInputName">Name</label>
-                    <input type="Name" class="form-control" size="30" id="name">
-                    <br>
-                    <label for="exampleInputEmail">Email</label>
-                    <input type="password" class="form-control" size="30" id="email">
-                    <br>
-                    <label for="exampleInputPhone">Phone No</label>
-                    <input type="password" class="form-control" size="30" id="Phone">
-                </form>
-              </div>
-              <div class="vl"></div>
-              <div id="Frame-2"><b>Message</b>
-                <br>
-                <p>Leave your message</p>
-                <br><br><br><br>
-                <a href="" class="btn btn-primary-3">Send Message</a>
-              </div>
-              <div class="text-right">
-          </div>
-            </div>
-          </div>
-        </div>
-        <div class="section2-image3">
-      </div>
-    </section>       
+              <form>
+                <div class="form-group">
+                  <label for="exampleInputName">Name</label>
+                  <input type="Name" class="form-control" size="30" id="name">
+                  <br>
+                  <label for="exampleInputEmail">Email</label>
+                  <input type="password" class="form-control" size="30" id="email">
+                  <br>
+                  <label for="exampleInputPhone">Phone No</label>
+                  <input type="tel" class="form-control" size="30" id="Phone">
+                </div>
+              </form>
 
+              <div class="vl"></div>
+                <div id="Frame-2"><b>Message</b>
+                  <br>
+                  <p>Leave your message</p>
+                  <br><br><br><br>
+                  <a href="" class="btn btn-primary-3">Send Message</a>
+                </div>
+
+              <div class="text-right"></div>
+        </div>
+              <div class="section2-image3"></div>
+      </div>
+    </section>
   </body>
 </html>
