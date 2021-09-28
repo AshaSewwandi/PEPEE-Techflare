b893a3bbf88e75e50f8d1574c536d91f139cbf6e
Sewwandi97 <asha.18@itfac.mrt.ac.lk>
2021-09-28 14:39:14 +0530

    DT-11-landing page completed

---
+++ app/Models/message.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'id', 'name', 'email', 'contact_number', 'message', 'user_id',];
+    protected $table = 'messages';
+    protected $primaryKey = 'id';
+
+    public $timestamp=false;
+
+}
