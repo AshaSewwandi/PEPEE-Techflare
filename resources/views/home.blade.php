39da8ad65d59f1d115e7d096b249504ee578acbe
lakminifernando77 <lakminifernando77@gmail.com>
2021-10-07 09:13:37 +0530

    DT-14- Add Post Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 <div class="container">
     <div class="caption text-center text-white" data-stellar-ratio="0.7">
         <section>
-             <div class="rightcolumn">
+            <div class="rightcolumn">
                 <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
-                  <div class="container bootstrap snippets bootdey">
+                    <div class="container bootstrap snippets bootdey">
                       <di class="col-md-8">
                           <div class="col-sm-12">
                               <div class="panel panel-white post panel-shadow">
@@ -26,9 +26,9 @@
                               </div> 
                           </div>
                       </di>
-                  </div>
-              </div>
-      </section>
+                    </div>
+            </div>
+        </section>
             <!-- <div class="card">
                 <div class="container1">
                     <img src="/img/profile.jpg"  class="avatar" alt="Annie" >
@@ -44,29 +44,107 @@
                 </div>
                 
              </div>  -->
-
             <div class="pull-left container1">
                 <div class="card" >
                     <div class="card-head">
-                    <img class="avatar" src="/img/profile.jpg" alt="Card image" >
-                    <h2><i>{{ Auth::user()->name }} </i></h2>
-                    <br>
+                        <img class="avatar" src="/img/profile.jpg" alt="Card image" >
+                        <h2><i>{{ Auth::user()->name }} </i></h2>
+                        <br>
                     </div>
                     <div class="card-body">
                         
                         <p>Name : <br>{{ Auth::user()->name }}</p>
-                       
+                        
                         <p>Email : {{ Auth::user()->email }} </p>
+
                         <p>Contact :{{ Auth::user()->MobileNo }} </p>
-                        
-                            <a  href="" class="btn btn-primary-3"> Edit Profile</a>
-                            <a  href="" class="btn btn-primary-4"> Add New Post</a>
-                        
+                            
+                        <a  href="" class="btn btn-primary-3"> Edit Profile</a>
+
+                        <button type="button" class="btn btn-primary-4" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Add New Post</button>
+ 
                     </div>
                 </div> 
+            </div>   
+    </div>
+</div>
+     
+@endsection    
+
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><b>Add Post</b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-
-
+            
+            <div class="modal-body">
+                <form method="POST" action="">
+                    @csrf
+                    <div class="form-group row">
+                        <label for="input1" class="col-sm-3 col-form-label" ><b>Type of the pet:</b> </label>
+                        <div class="col-sm-6">
+                        <select name="pet" class="custom-select" required>
+                            <option value=""  selected >Select the type</option>
+                            <option value="Dog">Dog</option>
+                            <option value="Cat">Cat</option>
+                        </select>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group row">
+                        <label for="input" class="col-sm-3 col-form-label"><b>Upload Image:</b></label>
+                        <div class="col-sm-6">
+                            <input type="file" class="form-control" id="input" >
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group row">
+                        <label for="input3" class="col-sm-3 col-form-label"><b>Write a caption:</b></label>
+                        <div class="col-sm-9">
+                            <textarea rows="4" cols="50" name="comment" id="usrform"></textarea>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group row">
+                        <label for="input3" class="col-sm-3 col-form-label"><b>Add Location:</b></label>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" id="input3">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="input4" class="col-sm-3 col-form-label"><b>Age:</b></label>
+                        <div class="col-sm-3">
+                            <input type="password" class="form-control" id="input4" >
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group row">
+                        <label for="input4" class="col-sm-2 col-form-label"><b>Gender:</b></label>
+                        <div class="col-sm-3">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+                        <label class="form-check-label" for="exampleRadios1">Male</label>
+                        <br>
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                        <label class="form-check-label" for="exampleRadios1">Female</label>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group row">
+                        <div class="checkbox">
+                           <b>Vaccinated: </b>   <label><input type="checkbox"> </label>
+                        </div>
+                    </div>
+                    <button type="button" class="btn btn-primary-1" data-bs-dismiss="modal">Add Post</button>
+                    <button type="submit" class="btn btn-primary-2" value="Submit">Cancel</button>
+                </form>
+            </div>       
         </div>
-    </div>  
-@endsection
+    </div>
+</div>
+
+ 
