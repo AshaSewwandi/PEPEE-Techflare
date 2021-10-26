a18317f69b73b9766d0945516ad0b6920513a5fa
Samadhi Jayawardena <jayawardena141@gmail.com>
2021-10-26 18:03:51 +0530

    DT-31-View Post Based on User ID

--- resources/views/addnewpost.blade.php
+++ resources/views/addnewpost.blade.php
@@ -1,31 +1,30 @@
 @extends('layouts.app')
 @section('content')
 <section>
-        <div class="rightcolumn">
-                    <div class="container bootstrap snippets bootdey">
-                      <di class="col-md-8">
-                          <div class="col-sm-12">
-                              <div class="panel panel-white post panel-shadow">
-                                  <div class="post-heading">
-                                      <div class="pull-left image">
-                                            @if(Auth::user()->image)
-                                            <img src=" {{ asset ('storage/images/users/' . Auth::user()->image) }}" alt="user profile image" class="img-circle avatar" >
-                                            @else
-                                            <img src="/img/profile.png" alt="user profile image" class="img-circle avatar">
-                                            @endif   
-                                      </div>
-                                      <div class="pull-right meta">
-                                      <a><i onclick="myFunction()" class="material-icons"style="font-size:48px">&#xe5d3;</i></a>
-                                        
-                                            <div class="dropdown"> 
-                                                <div id="myDropdown" class="dropdown-content">
-                                                    <a href="#home">Delete post</a>
-                                                    <a href="#about">Edit post</a>
-                                                </div>
-                                            </div>
-
-                                            <script>
-                                                function myFunction() {
+    <div class="container bootstrap snippets bootdey">
+        <div class="col-md-4">
+                    @foreach($posts as $post) 
+                        <div class="panel panel-white post panel-shadow">
+                            <div class="post-heading">
+                                <div class="pull-left image">
+                                    @if(Auth::user()->image)
+                                        <img src=" {{ asset ('storage/images/users/' . Auth::user()->image) }}" alt="user profile image" class="img-circle avatar" >
+                                    @else
+                                        <img src="/img/profile.png" alt="user profile image" class="img-circle avatar">
+                                    @endif   
+                                </div>
+                                <div class="pull-right meta">
+                                    <a>
+                                        <i onclick="myFunction()" class="material-icons"style="font-size:48px">&#xe5d3;</i>
+                                    </a>                                       
+                                    <div class="dropdown"> 
+                                        <div id="myDropdown" class="dropdown-content">
+                                            <a href="#home">Delete post</a>
+                                            <a href="#about">Edit post</a>
+                                        </div>
+                                    </div>
+                                        <script>
+                                            function myFunction() {
                                                 document.getElementById("myDropdown").classList.toggle("show");
                                                 }
                                                 window.onclick = function(event) {
@@ -40,28 +39,24 @@
                                                     }
                                                 }
                                                 }
-                                            </script>
-                                      </div>
-                                  </div>
-                                  <div class="post-image">
-                                      <form action="" method="get">
-                                        <!-- <img src="/img/cat1.jpg" class="image" alt="image post">  -->
-                                        @foreach($posts as $post) 
-                                        <input type="hidden" name="id" value="{{ Auth::user()->id }}" > 
-                                        <input type="hidden" name="user_id" value="{{ $post->user_id }}" > 
-                                            <img src=" {{ asset ('storage/images/posts/' . $post->image) }}" alt="Card image" class="image" >  
-                                        @endforeach
-                                        
-                                      </form>
-                                                
-                                  </div>
-                                  <div class=" post-description">
-                                      <h4><a href="#">View Post</a></h4>
-                                  </div>    
-                              </div> 
-                          </div>
-                      </di>
-                    </div>
+                                        </script>
+                                    </div>
+                                </div>
+
+                                <div class="post-image">
+                                    <!-- <img src="/img/cat1.jpg" class="image" alt="image post">  -->
+                                    <input type="hidden" name="id" value="{{ Auth::user()->id }}" > 
+                                    <input type="hidden" name="user_id" value="{{ $post->user_id }}" > 
+                                    <img src=" {{ asset ('storage/images/posts/' . $post->image) }}" alt="Card image" class="image" >                                                                                       
+                                </div>
+                                <div class=" post-description">
+                                    <h4><a href="#">View Post</a></h4>
+                                </div>    
+                            </div>    
+                        @endforeach
+                    </div> 
             </div>
-        </section>
+        </div>
+    </div>
+</section>
 @endsection
